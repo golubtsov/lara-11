e3680b44ee07b0c109d29c1c5f5e5b27fd4ef09c
n.golubtsov <n.golubtsov@els24.com>
2024-07-03 17:46:10 +0700

    * run test from app folder

---
+++ app/Domain/Users/Tests/Feature/UserControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Domain\Users\tests\Feature;
+
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    public function testIndex(): void
+    {
+        self::assertTrue(true);
+    }
+
+    public function testShow(): void
+    {
+        self::assertTrue(true);
+    }
+}
